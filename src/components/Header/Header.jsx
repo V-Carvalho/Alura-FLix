89aa45f92438d6ba1346c60e89f440df504a561e
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-04 10:55:14 -0300

    Add condicional de visualização no botão novo vídeo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import logo from "../../assets/images/logo.png";
 import Button from "../Button/Button";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const StyledHeader = styled.header`
   top: 0;
@@ -14,7 +14,7 @@ const StyledHeader = styled.header`
   position: fixed;
   align-items: center;
   justify-content: space-between;
-  border-bottom: 2px solid #2A7AE4;
+  border-bottom: 2px solid #2a7ae4;
   background: rgba(0, 0, 0, 1);
   box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.5);
 `;
@@ -25,23 +25,29 @@ const Logo = styled.img`
 `;
 
 const Header = () => {
+  const pageLocation = useLocation();
+  console.log(pageLocation);
   return (
     <StyledHeader>
       <Link to="/">
-        <Logo src={logo} alt="Logo SEGEL FLIX" />  
+        <Logo src={logo} alt="Logo SEGEL FLIX" />
       </Link>
 
-      <Link to="/newvideo">
-        <Button
-          height="50px"
-          width="180px"
-          fontSize="20px" 
-          title="Novo vídeo" 
-          titleColor="#f5f5f5" 
-          borderColor="#f5f5f5" 
-          backgroundColor="#000000" 
-        />
-      </Link>     
+      {pageLocation.pathname === "/" ? (
+        <Link to="/newvideo">
+          <Button
+            height="50px"
+            width="180px"
+            fontSize="20px"
+            title="Novo vídeo"
+            titleColor="#f5f5f5"
+            borderColor="#f5f5f5"
+            backgroundColor="#000000"
+          />
+        </Link>
+      ) : (
+        ""
+      )}
     </StyledHeader>
   );
 };
