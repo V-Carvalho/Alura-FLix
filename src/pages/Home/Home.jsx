cd64428b61092ec394e73a1270c41bddcb5166db
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-04 20:16:26 -0300

    Add função que recebe os dados a serem usados na criação da tela

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import Banner from "../../components/Banner/Banner"
 import ListCategories from "../../components/ListCategories/ListCategories"
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const Main = styled.main`
   display: flex;
@@ -25,8 +41,26 @@ const ContainerListCategories = styled.section`
   justify-content: center;
 `;
 
-// TODO: Pegar os dados a ser usado na home aqui
 const Home = () => {
+  const [listCategories, setListCategories] = useState([]);
+
+  const getAllCategories = () => {
+    onSnapshot(collection(db, "segel-flix"), (categories) => {
+      const data = categories.docs.map((category) => ({
+        id: category.id,
+        categoryName: category.data().categoryName,
+        categoryDescription: category.data().categoryDescription,
+        categoryColor: category.data().categoryColor, 
+        listVideos: category.data().listVideos
+      }));
+      setListCategories(data);
+    });
+  }
+
+  useEffect(() => {
+    getAllCategories();
+  }, []);
+
   return (
     <Main>
       <ContainerBanner>
@@ -34,7 +68,7 @@ const Home = () => {
       </ContainerBanner>
 
       <ContainerListCategories>
-        <ListCategories />
+        <ListCategories listCategories={listCategories}  />
       </ContainerListCategories>
     </Main>
   );
