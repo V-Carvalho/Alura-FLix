82d9a94c9c91d813e3ee09169a087dd00214f951
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-04 20:14:57 -0300

    Criada uma props p/ receber os dados usados na construção do componente

--- src/components/ListCategories/ListCategories.jsx
+++ src/components/ListCategories/ListCategories.jsx
@@ -1,21 +1,6 @@
 import { Fragment, useEffect, useState } from "react";
 import styled from "styled-components";
 import SliderVideos from "../SliderVideos/SliderVideos";
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, onSnapshot } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 const StyledListCategories = styled.div`
   z-index: 1;
@@ -58,42 +43,26 @@ const TagDescription = styled.p`
   justify-content: center;
 `;
 
-const ListCategories = () => {
-  const [categoryData, setCategoryData] = useState([]);
-
-  const getAllCategories = () => {
-    onSnapshot(collection(db, "segel-flix"), (categories) => {
-      const data = categories.docs.map((category) => ({
-        data: category.data(),
-        id: category.id,
-      }));
-      setCategoryData(data);
-    });
-  }
-
-  useEffect(() => {
-    getAllCategories();
-  }, []);
-
+const ListCategories = ({ listCategories }) => { 
   return (
     <StyledListCategories>
       <List>
-        {categoryData.map((category) =>
-          category.data.listVideos.length == 0 ? "" : (
-            <Fragment key={category.data.id}>
+        {listCategories.map((category) =>
+          category.listVideos.length == 0 ? "" : (
+            <Fragment key={category.id}>
               <ContainerTags>
-                <TagCategory backgroundCcolor={category.data.categoryColor}>
-                  {category.data.categoryName} 
+                <TagCategory backgroundCcolor={category.categoryColor}>
+                  {category.categoryName} 
                 </TagCategory>
 
                 <TagDescription >
-                  {category.data.categoryDescription} 
+                  {category.categoryDescription} 
                 </TagDescription>
               </ContainerTags> 
               
               <SliderVideos
-                listVideos={category.data.listVideos}
-                categoryColor={category.data.categoryColor}
+                listVideos={category.listVideos}
+                categoryColor={category.categoryColor}
               />
             </Fragment>
           )
