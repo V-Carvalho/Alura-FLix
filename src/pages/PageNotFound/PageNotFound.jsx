a41c5ccca06866f5d47aca9b9eac0c80d3f49be6
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-04 21:18:28 -0300

    Tela --> Página não encontrada finalizada

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,24 @@
-const { default: styled } = require("styled-components")
+import styled from "styled-components";
+import imgErrorMessag from "../../assets/images/page_not_found.png";
+
+const Main = styled.main`
+  display: flex;
+  margin-top: 94px;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ImgErrorMessag = styled.img`
+  max-width: 100%;
+`;
 
 const PageNotFound = () => {
-  return null
-}
+  return (
+    <Main>
+      <ImgErrorMessag src={imgErrorMessag} alt="Página não encontrada" />
+    </Main>
+  );
+};
 
 export default PageNotFound;
